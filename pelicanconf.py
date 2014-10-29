b71931bdd970ebecb9f228c91625e80935ea1993
Rahul Dave <rahuldave@gmail.com>
2014-10-29 15:06:48 -0400

    small changes

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
+from sitestuff import *
 
-AUTHOR = u'Author'
-SITENAME = u'Site'
+AUTHOR = u'Professor'
+SITENAME = u'Class'
 SITEURL = ''
 
 PATH = 'content'
@@ -23,17 +24,7 @@ THEME = 'support/themes/modpbs3'
 BOOTSTRAP_THEME = 'flatly'
 TWITTER_USERNAME="Author"
 
-# Blogroll
-LINKS =  (('Piazza', 'https://piazza.com/harvard/spring2014/am207/home'),
-          ('Videos', 'http://cm.dce.harvard.edu/2014/02/24104/publicationListing.shtml'),
-          ('ISite', 'http://isites.harvard.edu/icb/icb.do?keyword=k102129&login=yes'),)
 
-OTHERLINKS = (('Numpy', 'http://docs.scipy.org/doc/numpy/reference/'),
-          ('Scipy', 'http://docs.scipy.org/doc/scipy/reference/'),
-          ('Pandas', 'http://pandas.pydata.org/pandas-docs/dev/'),
-          ('Matplotlib', 'http://matplotlib.org/api/index.html'),
-          ('PyMC3', 'https://github.com/pymc-devs/pymc'),
-          ('IACS', 'http://iacs.seas.harvard.edu'),)
 
 # Social widget
 #SOCIAL = True
